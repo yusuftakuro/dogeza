10e18ae8545d153b332841a0b6e970ada79cc9c5
yusuftakuro <cvfthbv1@gmail.com>
2026-09-25 18:29:36 +0900

    Lower head into final dogeza depth

--- study.js
+++ study.js
@@ -225,10 +225,10 @@ function poseTorsoLean(){
 function poseTorsoDogeza(){
   // Pelvis stays over the heels; the trunk progresses forward and down.
   // Roll is locked by aimBone's anatomical side-frame constraint.
-  aimBone('spine','spine1',V(0,.16,.987));
-  aimBone('spine1','spine2',V(0,-.06,.998));
-  aimBone('spine2','neck',V(0,-.30,.954));
-  aimBone('neck','head',V(0,-.64,.768));
+  aimBone('spine','spine1',V(0,.08,.997));
+  aimBone('spine1','spine2',V(0,-.16,.987));
+  aimBone('spine2','neck',V(0,-.42,.907));
+  aimBone('neck','head',V(0,-.78,.626));
 }
 
 function poseHandsOnThighs(){
